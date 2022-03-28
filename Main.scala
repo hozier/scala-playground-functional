d04163d72dfefc42b399f1543afc3d597125c26e
P. William Hozier <hozier@users.noreply.github.com>
2022-03-28 10:47:55 -0400

    Develop: Implement Exercises and restructure root. (#1)
    * Implement recursive function which verifies the balancing of parentheses.
    
    * Fmt main
    
    * Housekeeping
    
    * Restructure directory, mv .git to its parent.
    
    * mv README.md to root
    
    Co-authored-by: P William Hozier <philron.hozier@modaoperandi.com>
    Co-authored-by: P. William Hozier <github.com/hozier>

--- Main.scala
+++
@@ -1,6 +0,0 @@
-object Main extends App {
-
-  /** execute driver(s)
-    */
-  Functional.Operations.main()
-}
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,7 @@
+object Main extends App {
+
+  /** execute driver(s)
+    */
+  Fundamentals.Operations.main()
+  Exercises.Operations.main()
+}
