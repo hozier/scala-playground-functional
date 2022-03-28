f86a999be2e0c951322aa5a53094ca91b50f3953
P William Hozier <philron.hozier@modaoperandi.com>
2022-03-28 10:09:27 -0400

    Restructure directory, mv .git to its parent.

--- src/main/scala/concepts/Fundamentals.scala
+++ src/main/scala/concepts/Fundamentals.scala
@@ -1,4 +1,4 @@
-object Fundamentals {
+object Fundamentals extends App {
   object Operations {
     def doSum(in: List[Int]): Int = in.fold(0)((acc, item) => acc + item)
     def countFizz(universe: List[Int], FIZZ: Int = 3): Int = {
